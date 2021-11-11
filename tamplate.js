b9ece24390b6c2935bd4a4fa75f7c946d3b7c47a
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-11 17:43:38 +0600

    Caret symbol using

---
+++ tamplate.js
@@ -0,0 +1,14 @@
+const firstName = "Justin";
+const lastName = "Timberlake";
+
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName} is a good singer `;
+
+const multiLine = "i love you " + "I miss you " + "I need you"; // `` caret symbol use for removing code line decrease . previous we use + +  for Space . just like this line
+
+const multiLine2 = `I love you .
+I miss you .
+I need you `; // we can use this line instead of upper line
+console.log(multiLine);
+console.log(multiLine2);
+console.log(fullName2);
