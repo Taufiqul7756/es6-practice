45b5ec67efaf3c6953ab2de1bdc32eff2061fc59
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-11-13 22:56:41 +0600

    Class, Object creation and constructor

---
+++ class.js
@@ -0,0 +1,11 @@
+class student {
+  constructor(sId, sName) {
+    this.id = sId;
+    this.name = sName;
+    this.school = "BSJA";
+  }
+}
+const student1 = new student(12, "Shuvo");
+const student2 = new student(22, "mahiya");
+const student3 = new student(23, "Bappi");
+console.log(student3);
